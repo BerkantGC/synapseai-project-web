9b4cb4c416147186026a3c646218cf32940f80cd
berkantgc <21070001009@stu.yasar.edu.tr>
2025-01-19 18:36:56 +0300

    minor changes

--- app/head.js
+++ app/head.js
@@ -2,6 +2,7 @@ const Head = () => (
   <>
     <title>Synapse AI</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Synapse | AI-Enhanced Social Media Platform" />
     <link rel="icon" href="/synapse-icon.png" />
   </>
 );
